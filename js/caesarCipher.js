1fb15e7978bb0508e805cef25294f6570268e3ca
KevinBaoB <kevinbaobui@gmail.com>
2022-06-01 16:41:48 -0500

    Second

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,6 +1,4 @@
-// exports.
-// 64 < alphabets > 91
-caesarCipher = function (str, num) {
+exports.caesarCipher = function (str, num) {
   let arr = [];
   for (let i in str) {
     if (!/^[a-z]+$/i.test(str[i])) {
@@ -13,6 +11,9 @@ caesarCipher = function (str, num) {
         const correct = 64 + (newPos - 90);
 
         arr.push(String.fromCharCode(correct));
+      } else if (newPos < 65) {
+        const negCorrect = 91 - (65 - newPos);
+        arr.push(String.fromCharCode(negCorrect));
       } else {
         arr.push(String.fromCharCode(newPos));
       }
@@ -23,6 +24,9 @@ caesarCipher = function (str, num) {
         let correct = 96 + (newPos - 122);
 
         arr.push(String.fromCharCode(correct));
+      } else if (newPos < 97) {
+        let negCorrect = 123 - (97 - newPos);
+        arr.push(String.fromCharCode(negCorrect));
       } else {
         arr.push(String.fromCharCode(newPos));
       }
@@ -32,5 +36,7 @@ caesarCipher = function (str, num) {
   return arr.join("");
 };
 
-console.log(caesarCipher("What", 5));
-console.log(caesarCipher("What a string!", 5));
+// console.log(caesarCipher("What", 5));
+// console.log(caesarCipher("What a string!", 5));
+// console.log(caesarCipher("Boy! What a string!", -5));
+// console.log("Wjt! Rcvo v nomdib!");
