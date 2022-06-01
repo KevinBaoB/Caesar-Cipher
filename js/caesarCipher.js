1ac5507080704fe78bc668338b4698f6b8185d4a
KevinBaoB <kevinbaobui@gmail.com>
2022-06-01 15:48:10 -0500

    First

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,36 @@
-exports.caesarCipher = function() {
+// exports.
+// 64 < alphabets > 91
+caesarCipher = function (str, num) {
+  let arr = [];
+  for (let i in str) {
+    if (!/^[a-z]+$/i.test(str[i])) {
+      arr.push(str[i]);
+    }
+    const alphaNum = str.charCodeAt(i);
+    if (alphaNum > 64 && alphaNum < 91) {
+      const newPos = alphaNum + num;
+      if (newPos > 90) {
+        const correct = 64 + (newPos - 90);
 
+        arr.push(String.fromCharCode(correct));
+      } else {
+        arr.push(String.fromCharCode(newPos));
+      }
+    } else if (alphaNum > 96 && alphaNum < 123) {
+      let newPos = alphaNum + num;
+
+      if (newPos > 122) {
+        let correct = 96 + (newPos - 122);
+
+        arr.push(String.fromCharCode(correct));
+      } else {
+        arr.push(String.fromCharCode(newPos));
+      }
+    }
+  }
+
+  return arr.join("");
 };
+
+console.log(caesarCipher("What", 5));
+console.log(caesarCipher("What a string!", 5));
